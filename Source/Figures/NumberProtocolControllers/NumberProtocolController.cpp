d8ea3108b2633190d1adbce73da05bebb438704b
Simon Pratt <simonjamespratt@gmail.com>
2021-01-14 09:33:52 +0000

    wip

--- Source/Figures/NumberProtocolControllers/NumberProtocolController.cpp
+++ Source/Figures/NumberProtocolControllers/NumberProtocolController.cpp
@@ -1,5 +1,11 @@
 #include "NumberProtocolController.h"
 
+#include "AdjacentStepsProtocolController.h"
+#include "BasicProtocolController.h"
+#include "CycleProtocolController.h"
+
+#include <stdexcept>
+
 void NumberProtocolController::attach(Observer observer)
 {
     observers.emplace_back(observer);
@@ -10,5 +16,24 @@ void NumberProtocolController::notifyParamsChanged()
     auto newParams = getParams();
     for(auto const &observer : observers) {
         observer(newParams);
+    }
+}
+
+std::unique_ptr<NumberProtocolController>
+NumberProtocolController::create(Type type, Params params)
+{
+    switch(type) {
+    case Type::adjacentSteps:
+        return std::make_unique<AdjacentStepsProtocolController>();
+        break;
+    case Type::basic:
+        return std::make_unique<BasicProtocolController>();
+        break;
+    case Type::cycle:
+        return std::make_unique<CycleProtocolController>(params);
+        break;
+
+    default:
+        throw std::invalid_argument("Type not recognised");
     }
 }
--- Source/Figures/NumberProtocolControllers/NumberProtocolController.h
+++ Source/Figures/NumberProtocolControllers/NumberProtocolController.h
@@ -1,12 +1,15 @@
 #pragma once
+#include <NumberProtocol.hpp>
 #include <NumberProtocolParameters.hpp>
 #include <functional>
 #include <juce_gui_basics/juce_gui_basics.h>
+#include <memory>
 #include <vector>
 
 class NumberProtocolController : public juce::Component {
-    using Observer = std::function<void(
-        aleatoric::NumberProtocolParameters::Protocols newParams)>;
+    using Params = aleatoric::NumberProtocolParameters::Protocols;
+    using Observer = std::function<void(Params newParams)>;
+    using Type = aleatoric::NumberProtocol::Type;
 
   public:
     virtual void
@@ -20,6 +23,10 @@ class NumberProtocolController : public juce::Component {
     void attach(Observer observer);
 
     void notifyParamsChanged();
+
+    // Factory
+    static std::unique_ptr<NumberProtocolController> create(Type type,
+                                                            Params params);
 
   private:
     std::vector<Observer> observers;
